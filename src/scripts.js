4de8bc2de806ccc98139ec2f4af356e6c826faa0
AetherEncoder <saikopewds@gmail.com>
2024-11-01 20:21:14 +0800

    Javascript update

--- src/scripts.js
+++ src/scripts.js
@@ -59,11 +59,12 @@ function homeChangePic(theme){
     }
 
     else{
+        document.getElementById('logo').src = 'src/images/CCS_Logo.png';
+        document.getElementById('icon').href = 'src/images/CCS_logo.png';
         document.getElementById('lanyard').src = 'src/images/lanyard_announcement.png';
         document.getElementById('tryout').src = 'src/images/tryout_announcement.png';
         document.getElementById('teacher').src = 'src/images/teachers_day.png';
-        document.getElementById('logo').src = 'src/images/CCS_Logo.png';
-        document.getElementById('icon').href = 'src/images/CCS_logo.png';
+        
     }
 }
 
